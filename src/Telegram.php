3ed81de4361dc7de4a950e7b34743f1857f1ff6d
Alexey Lee <alex@plumy.ru>
2020-12-22 19:34:21 +0600

    Fix building Update

--- src/Telegram.php
+++ src/Telegram.php
@@ -86,22 +86,27 @@ class Telegram extends Api
     {
         $data = json_decode(file_get_contents('php://input'), true);
 
-        if ($this->update = new Update($data)) {
-            $updateId = $this->update->update_id;
-            if (!$result = $this->db->where('_id', '=', 1)->fetch()) {
-                $this->db->insert(['update_id' => 0]);
-                $result[0]['update_id'] = 0;
-            }
-            // Run command if the update is new
-            if ($result[0]['update_id'] < $updateId) {
-                $this->db->where('_id', '=', 1)->update(['update_id' => $updateId]);
-                if ($debug && $this->adminId) {
-                    $this->chatId = '117780107';
-                    $this->sendMessage('<pre>' . json_encode($data) . '</pre>');
+        try {
+            if ($this->update = new Update($data)) {
+                $updateId = $this->update->update_id;
+                if (!$result = $this->db->where('_id', '=', 1)->fetch()) {
+                    $this->db->insert(['update_id' => 0]);
+                    $result[0]['update_id'] = 0;
                 }
-                $this->chatId = $this->update->user->id;
-                $this->runCommand();
+                // Run command if the update is new
+                if ($result[0]['update_id'] < $updateId) {
+                    $this->db->where('_id', '=', 1)->update(['update_id' => $updateId]);
+                    if ($debug && $this->adminId) {
+                        $this->chatId = '117780107';
+                        $this->sendMessage('<pre>' . json_encode($data) . '</pre>');
+                    }
+                    $this->chatId = $this->update->user->id;
+                    $this->runCommand();
+                }
             }
+            return true;
+        } catch (\Throwable $e) {
+            return $e;
         }
     }
 
