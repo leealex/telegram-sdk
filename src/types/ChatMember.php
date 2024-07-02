9c211263973762a88907f3b877763b80c4281599
leealex <alex@plumy.ru>
2024-07-02 12:29:57 +0500

    Fix

--- src/types/ChatMember.php
+++ src/types/ChatMember.php
@@ -11,6 +11,12 @@ namespace leealex\telegram\types;
  * @property User $user Information about the user
  *
  * @property string $status The member's status in the chat
+ *
+ * @property string $is_anonymous True, if the user's presence in the chat is hidden
+ *
+ * @property string $custom_title Optional. Custom title for this user
+ *
+ * @property integer $until_date Date when restrictions will be lifted for this user; Unix time. If 0, then the user is banned forever
  */
 class ChatMember extends BaseType
 {
